cd1089baf4b454f64738a2f7f0e4745f38d4f9ac
cshroff <cshroff1@sheffield.ac.uk>
2017-03-21 23:00:18 +0000

    moved script to js page

---
+++ ourscript.js
@@ -0,0 +1,14 @@
+
+$( "#Judit" ).hover(function() {
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 100 );
+});
+$( "#claire" ).hover(function() {
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 100 );
+});$( "#Tan" ).hover(function() {
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 100 );
+});
+
+
